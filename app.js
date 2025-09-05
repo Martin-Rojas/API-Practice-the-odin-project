ac52d88afa792ab5234ea738af55db4f1e4068ec
Martin-Rojas <rojas.martin25@yahoo.com>
2025-09-04 18:24:11 -0700

    code btn get new image every click

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// kNGRrtwl8BL1ekkGbhTRnDXkBP3y48M0
-
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=kNGRrtwl8BL1ekkGbhTRnDXkBP3y48M0&s=realmadrid"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+const btnNewImage = document.getElementById(`change-img`);
+
+function getImage() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=kNGRrtwl8BL1ekkGbhTRnDXkBP3y48M0&s=cats"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+// Initial image
+getImage();
+
+btnNewImage.addEventListener(`click`, getImage);
