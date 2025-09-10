7148fbdd5e4d143f3d16b7d9c1855874ec2cf4bf
Martin-Rojas <rojas.martin25@yahoo.com>
2025-09-09 17:25:53 -0700

    code update to catch an error when get data from API

--- app.js
+++ app.js
@@ -7,19 +7,34 @@ function getImage(searchTerm = `cats`) {
   fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=kNGRrtwl8BL1ekkGbhTRnDXkBP3y48M0&s=${searchTerm}`
   )
-    .then(function (response) {
+    .then((response) => {
       return response.json();
     })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
+    .then((data) => {
+      // Check if data is coming through
+      if (
+        data.data &&
+        data.data.images &&
+        data.data.images.original &&
+        data.data.images.original.url
+      ) {
+        img.src = data.data.images.original.url;
+      } else {
+        img.src = `errorImg.avif`;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching GIF:", error.message);
     });
 }
 
 // Initial image
 getImage();
 
+// Btn to get new image
 btnNewImage.addEventListener(`click`, getImage);
 
+// Get a term and get an image
 formEl.addEventListener(`submit`, (event) => {
   event.preventDefault();
   getImage(searchBoxValue.value);
